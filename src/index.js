38732705341661a2ed053222776e6f8a313763b8
Xavier Guarch <xavier.guarch@safelayer.com>
2018-07-04 01:03:27 -0300

    Añadida librería Bootstrap (4.1.1) e incorporado su CSS en index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
